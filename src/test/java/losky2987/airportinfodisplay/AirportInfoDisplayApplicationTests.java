eff9a642ed23bcde3848429c34812d6aa85be7f2
losky2987 <losky2987@gmail.com>
2025-04-25 11:21:29 +0200

    Springboot project init

---
+++ src/test/java/losky2987/airportinfodisplay/AirportInfoDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package losky2987.airportinfodisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportInfoDisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
